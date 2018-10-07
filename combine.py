4c517e857ba899a5233912874826869fa7c8be25
Martin Boyanov <mboyanov@gmail.com>
2018-10-07 18:56:36 +0300

    Combine the rnn layers and test

--- combine.py
+++ combine.py
@@ -11,12 +11,6 @@ def extract_embedding_layer(rnn_core):
 
 class CombinedLM(nn.Module):
 
-    def _combine_embedding(self, fwd, bwd):
-        combined_embedding = extract_embedding_layer(self.core)
-        fwd_embedding = extract_embedding_layer(fwd)
-        bwd_embedding = extract_embedding_layer(bwd)
-        combined_embedding.weight.data.set_(torch.cat((fwd_embedding.weight.data, bwd_embedding.weight.data), dim=1))
-
     def __init__(self, fwd: RNNCore, bwd: RNNCore):
         super().__init__()
         self.core = RNNCore(vocab_size, fwd.emb_sz + bwd.emb_sz,
@@ -25,24 +19,63 @@ class CombinedLM(nn.Module):
         self._combine_embedding(fwd, bwd)
         self._combine_rnns(fwd, bwd)
 
+    def _combine_embedding(self, fwd, bwd):
+        combined_embedding = extract_embedding_layer(self.core)
+        fwd_embedding = extract_embedding_layer(fwd)
+        bwd_embedding = extract_embedding_layer(bwd)
+        combined_embedding.weight.data.set_(torch.cat((fwd_embedding.weight.data, bwd_embedding.weight.data), dim=1))
+
     def _combine_rnns(self, fwd, bwd):
-        combined_rnn_0 = self.core.rnns[0]
-        fwd_rnn_0 = fwd.rnns[0]
-        bwd_rnn_0 = bwd.rnns[0]
-        fwd_weights = fwd_rnn_0.module.weight_ih_l0.data
-        bwd_weights = bwd_rnn_0.module.weight_ih_l0.data
-        combined_rnn_0.module.weight_ih_l0.data.set_(
-            torch.cat((
-                fwd_weights,
-                torch.zeros(fwd_weights.size())
-            ), dim =1)
-        )
-        combined_rnn_0.module.weight_ih_l0_reverse.data.set_(
-            torch.cat((
-                torch.zeros(bwd_weights.size()),
-                bwd_weights
-            ), dim=1)
-        )
+        for combined_rnn, fwd_rnn, bwd_rnn in zip(self.core.rnns, fwd.rnns, bwd.rnns):
+            ##FWD
+
+            fwd_weights = fwd_rnn.module.weight_ih_l0.data
+            bwd_weights = bwd_rnn.module.weight_ih_l0.data
+
+            combined_rnn.module.weight_ih_l0.data.set_(
+                torch.cat((
+                    fwd_weights,
+                    torch.zeros(fwd_weights.size())
+                ), dim=1)
+            )
+            combined_rnn.module.weight_ih_l0.data.set_(
+                torch.cat((
+                    fwd_weights,
+                    torch.zeros(fwd_weights.size())
+                ), dim=1)
+            )
+            combined_rnn.module.weight_hh_l0.data.set_(
+                fwd_rnn.module.weight_hh_l0.data
+            )
+
+            combined_rnn.module.bias_ih_l0.data.set_(
+                fwd_rnn.module.bias_ih_l0.data
+            )
+
+            combined_rnn.module.bias_hh_l0.data.set_(
+                fwd_rnn.module.bias_hh_l0.data
+            )
+
+            ## BWD
+
+            combined_rnn.module.weight_ih_l0_reverse.data.set_(
+                torch.cat((
+                    torch.zeros(bwd_weights.size()),
+                    bwd_weights
+                ), dim=1)
+            )
+
+            combined_rnn.module.weight_hh_l0_reverse.data.set_(
+                bwd_rnn.module.weight_hh_l0.data
+            )
+
+            combined_rnn.module.bias_ih_l0_reverse.data.set_(
+                bwd_rnn.module.bias_ih_l0.data
+            )
+
+            combined_rnn.module.bias_hh_l0_reverse.data.set_(
+                bwd_rnn.module.bias_hh_l0.data
+            )
 
     def forward(self, input):
         self.core.reset()
--- tests/test_combined_lstm.py
+++ tests/test_combined_lstm.py
@@ -7,7 +7,7 @@ from combine import CombinedLM
 vocab_size = 1000
 embedding_size = 3
 n_hid = 70
-n_layers = 1
+n_layers = 5
 
 
 @pytest.fixture
@@ -38,8 +38,29 @@ def test_should_combine_rnns(fwd, bwd):
     # GIVEN
     combined = CombinedLM(fwd, bwd)
     inp = torch.LongTensor([[1, 2, 3]]).t()
+    reversed_input = torch.LongTensor([[3, 2, 1]]).t()
     # WHEN
+    fwd.reset()
+    fwd.train(False)
+    fwd_outputs = fwd(inp)[1]
+
+    bwd.reset()
+    bwd.train(False)
+    bwd_outputs = bwd(reversed_input)[1]
+
     combined.train(False)
-    output = combined(inp)
+    output = combined(inp)[1]
+
     # THEN
     assert output is not None
+
+    for i in range(n_layers - 1):
+        np.testing.assert_array_almost_equal(output[i].detach().numpy()[:, :, :n_hid], fwd_outputs[i].detach().numpy())
+        bwd_expected = reversed(bwd_outputs[i].detach()).numpy()
+        np.testing.assert_array_almost_equal(output[i].detach()[:, :, n_hid:], bwd_expected)
+
+    np.testing.assert_array_almost_equal(output[-1].detach().numpy()[:, :, :embedding_size],
+                                  fwd_outputs[-1].detach().numpy())
+    bwd_expected = reversed(bwd_outputs[-1].detach()).numpy()
+    np.testing.assert_array_almost_equal(output[-1].detach().numpy()[:, :, embedding_size:],
+                                  bwd_expected)
